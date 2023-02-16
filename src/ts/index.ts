300045c1420bca47a1a1923034bc3da9e1facfbf
SoftAnnaLee <jazzchamp600@gmail.com>
2023-02-15 22:36:07 -0700

    start cli interface

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,18 +1,3 @@
-import '../scss/main.scss';
-import Icon from '../img/icon.png';
+// import '../scss/main.scss';
 
-function component() {
-  const content = document.getElementById('content');
-
-  content.innerText = `Hello webpack`;
-  content.classList.add('invert');
-
-  // Example of adding an image
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  content.appendChild(myIcon);
-
-  return content;
-}
-
-document.body.appendChild(component());
+console.log('Hello, world!');
